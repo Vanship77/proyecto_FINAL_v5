4cff4dd1034a81808a28e0692e8f114eba43d2b1
Rommel <cap03487@gmail.com>
2025-06-10 22:20:31 -0500

    xd

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -22,10 +22,17 @@ router.get('/login/google', passport.authenticate('google', { scope: ['profile',
 router.use('/', authRoutes);
 
 router.get('/logout', function(req, res, next) {
-  req.logout(function(err) {
-    if (err) { return next(err) }
-    res.redirect('/');
+  req.logout(err => {
+    if (err) {
+      console.error('Error cerrando sesión:', err);
+      return res.status(500).send('Error cerrando sesión');
+    }
+    req.session.destroy(() => {
+      res.clearCookie('connect.sid');
+      res.redirect('/');
+    });
   });
 });
+
 
 module.exports = router;//just for changes
